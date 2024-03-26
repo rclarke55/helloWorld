c9201ff720bb3a85429d1f6074c29518fb46b96d
Ryan Clarke <rclarke5@terpmail.umd.edu>
2024-03-26 18:26:27 -0400

    Created contact and favorite course html files that rely on the newly created base html file for design. Updated the app file to include new routes for contact and favorite course files.

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Ryan Clarke! I am adding my first code change.'
 
 @app.route('/hello')
-def hello():
+def hello():  # put application's code here
     return render_template('hello.html')
 
 @app.route('/about')
-def about():
+def about():  # put application's code here
     return render_template('about.html')
 
 @app.route('/about-css')
-def about_css():
+def about_css():  # put application's code here
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favorite_course():  # put application's code here
+    print('Course subject entered: ' + request.args.get('subject'))
+    print('Course number entered: ' + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():  # put application's code here
+    if request.method == 'POST':
+        return render_template('contact.html', submission=True)
+    else:
+        return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
